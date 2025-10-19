16fe13c101e202854a3daaf90bc08ed3e9d4a16d
Jack Kalsched <jkalsched@ucsd.edu>
2025-10-18 20:09:21 -0700

    nav 1.3

--- global.js
+++ global.js
@@ -6,6 +6,9 @@ function $$(selector, context = document) {
 
 const navLinks = $$("nav a");
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
 
 const currentLink = navLinks.find(
     (a) => a.host === location.host && a.pathname === location.pathname
@@ -28,6 +31,7 @@ document.body.prepend(nav);
 
 for (let p of pages) {
     let url = p.url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
     let title = p.title;
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
