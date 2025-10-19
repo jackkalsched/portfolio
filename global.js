0afa8726f186b3d20fe6993d2eda8e17d8e22a75
Jack Kalsched <jkalsched@ucsd.edu>
2025-10-18 21:05:24 -0700

    nav 1.12

--- global.js
+++ global.js
@@ -8,7 +8,7 @@ function $$(selector, context = document) {
 const BASE_PATH =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "/"
-    : "/portfolio/"; 
+    : "/"; 
 
 let pages = [
   { url: "", title: "Home" },
