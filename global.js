e0c7bd0d45af3e4ef8d50fac45a350cd307d9e78
Jack Kalsched <jkalsched@ucsd.edu>
2025-10-18 20:12:45 -0700

    nav 1.4

--- global.js
+++ global.js
@@ -4,21 +4,16 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
-
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
   : "/website/";         // GitHub Pages repo name
 
-const currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
 
   if (currentLink) {
     currentLink?.classList.add('current');
   }
 
-let pages = [
+const pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
     { url: 'resume/', title: 'Resume' },
@@ -29,9 +24,17 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-for (let p of pages) {
+for (const p of pages) {
     let url = p.url;
     url = !url.startsWith('http') ? BASE_PATH + url : url;
-    let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${p.title}</a>`);
   }
+
+const navLinks = $$("nav a");
+
+const currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+  );
+  
+  // Add the "current" class if found
+  currentLink?.classList.add("current");
