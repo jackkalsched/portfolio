48e2d00c2e73e54a2b4c27c786e6368768d59f3b
Jack Kalsched <jkalsched@ucsd.edu>
2025-10-18 21:38:56 -0700

    nav 1.21

---
+++ global.js
@@ -0,0 +1,34 @@
+console.log("IT’S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/"; 
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "resume/", title: "Resume" },
+  { url: "contact/", title: "Contact Me" },
+  { url: "https://github.com/jackkalsched", title: "GitHub" },
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
+}
+
+const navLinks = $$("nav a");
+
