e7e598548e64eabc3a234484e8684e25115b1b78
Jack Kalsched <jkalsched@ucsd.edu>
2025-10-31 10:32:39 -0700

    step 1.1 and 1.2

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from "../global.js";
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 
 async function initProjectsPage() {
   const projects = await fetchJSON("../lib/projects.json");
