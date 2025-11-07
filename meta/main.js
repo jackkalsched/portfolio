21f3625de85cddd89731db2ee86c6bd8a90919e4
Jack Kalsched <jkalsched@ucsd.edu>
2025-11-06 22:25:33 -0800

    steps 2.1 and 2.2

--- meta/main.js
+++ meta/main.js
@@ -97,9 +97,79 @@ function renderCommitInfo(data, commits) {
   addMetric('Most Productive Time of Day', mostWorkPeriod);
 }
 
+function renderScatterPlot(data, commits) {
+  // Define dimensions
+  const width = 1000;
+  const height = 600;
+
+  // Create the SVG
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  // Create scales
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+  // Define margins
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  // Define usable area
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  // Update scales with new ranges
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+  // Create the axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+  // Add X axis
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  // Add Y axis
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+  // Draw the scatter plot dots
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+
 let data = await loadData();
 let commits = processCommits(data);
 console.log(commits);
 
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
 
